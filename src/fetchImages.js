8e3a1362c1b86584569794bf9e27eed97d605f33
Olena Yatseniuk <elena.yatsenyuk@gmail.com>
2022-08-18 18:43:53 +0300

    test

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 const BASE_URL = 'https://pixabay.com/api';
 
 export async function fetchImages(value, page = 1) {
-  const searchParams = new URLSearchParams({
-    key: '29332799-a92cd8e6a78297bd57d7c923a',
-    q: value,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    page: page,
-    per_page: 40,
-  });
-  const response = await axios.get(`${BASE_URL}?${searchParams}`);
+  // const searchParams = new URLSearchParams({
+  //   key: '29332799-a92cd8e6a78297bd57d7c923a',
+  //   q: value,
+  //   image_type: 'photo',
+  //   orientation: 'horizontal',
+  //   safesearch: true,
+  //   page: page,
+  //   per_page: 40,
+  // });
+  const response = await axios.get(
+    `?q=value&key&29332799-a92cd8e6a78297bd57d7c923a&image_type=photo`
+  );
+  // const response = await axios.get(`${BASE_URL}?${searchParams}`);
   return response;
 }
